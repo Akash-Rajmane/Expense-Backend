55973de0928b1accc31db583131642c695a4a590
Akash <akashrajmane23@gmail.com>
2024-05-09 13:02:17 +0530

    minor changes

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password,
-     {dialect: 'mysql', host: process.env.host, port: process.env.DB_PORT})
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD,
+     {dialect: 'mysql', host: process.env.HOST, port: process.env.DB_PORT})
 
 module.exports = sequelize;
