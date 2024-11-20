b2f87b36fa2d4b0c5c1186d018952dbda5bb34b3
Akash <akashrajmane23@gmail.com>
2024-11-20 11:18:53 +0530

    minor change

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
-const cors = require('cors');
-const helmet = require('helmet');
+const cors = require("cors");
+const helmet = require("helmet");
 const userRoutes = require("./routes/user");
 const expenseRoutes = require("./routes/expense");
 const purchaseRoutes = require("./routes/purchase");
@@ -12,18 +12,27 @@ const premiumRoutes = require("./routes/premium");
 const passwordRoutes = require("./routes/password");
 const User = require("./models/user");
 const Expense = require("./models/expense");
-const Order = require('./models/order');
-const ForgotPasswordRequest = require('./models/forgotPassword');
+const Order = require("./models/order");
+const ForgotPasswordRequest = require("./models/forgotPassword");
 const ExpensesUrl = require("./models/expensesUrl");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 const app = express();
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(helmet());
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(bodyParser.json());
 
 // Relationships
@@ -44,15 +53,14 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use(userRoutes);
 app.use(expenseRoutes);
-app.use('/purchase', purchaseRoutes);
-app.use('/premium', premiumRoutes);
-app.use('/password', passwordRoutes);
+app.use("/purchase", purchaseRoutes);
+app.use("/premium", premiumRoutes);
+app.use("/password", passwordRoutes);
 
 sequelize.sync().then(() => {
-    app.listen(4000, () => {
-      console.log('Server is running on port 4000');
-    });
+  app.listen(4000, () => {
+    console.log("Server is running on port 4000");
   });
+});
